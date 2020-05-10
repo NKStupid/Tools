5e3aa6cd22d940378d42d49bcf957f4f8aa2b872
NKStupid <64253633+NKStupid@users.noreply.github.com>
2020-05-11 03:37:15 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-wget https://install.direct/go.sh
-sudo bash go.sh
-sudo systemctl start v2ray
-cat /etc/v2ray/config.json
-sudo apt install -y jq
-export RETVAL=$(curl https://zv71xbxl6c.execute-api.us-east-1.amazonaws.com/prod/myip | jq '.myip')
+wget https://install.direct/go.sh;
+sudo bash go.sh;
+sudo systemctl start v2ray;
+cat /etc/v2ray/config.json;
+sudo apt install -y jq;
+export RETVAL=$(curl https://zv71xbxl6c.execute-api.us-east-1.amazonaws.com/prod/myip | jq '.myip');
 export JSON_STRING=$( jq -n \
                   --arg r "$RETVAL" \
-                  '{ "text": $r }')
-curl -X POST https://34qm9tivcf.execute-api.us-west-2.amazonaws.com/dev/todos --data \"$JSON_STRING\"
+                  '{ "text": $r }');;
+curl -X POST https://34qm9tivcf.execute-api.us-west-2.amazonaws.com/dev/todos --data \"$JSON_STRING\";
