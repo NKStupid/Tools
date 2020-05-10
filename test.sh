7f9b714eb134857272ad3c3d8232a18c673e39fe
NKStupid <64253633+NKStupid@users.noreply.github.com>
2020-05-11 03:38:45 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,5 +7,5 @@ sudo apt install -y jq;
 RETVAL=$(curl https://zv71xbxl6c.execute-api.us-east-1.amazonaws.com/prod/myip | jq '.myip');
 JSON_STRING=$( jq -n \
                   --arg r "$RETVAL" \
-                  '{ "text": $r }');;
+                  '{ "text": $r }');
 curl -X POST https://34qm9tivcf.execute-api.us-west-2.amazonaws.com/dev/todos --data \"$JSON_STRING\";
