6678942e0737fd5c9dc52c0cd200830fcc88fb45
NKStupid <64253633+NKStupid@users.noreply.github.com>
2020-05-11 06:12:16 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,4 +7,4 @@ cat /etc/v2ray/config.json;
 export PORT=$(cat /etc/v2ray/config.json | jq '.inbounds[0] .port');
 export ID=$(cat /etc/v2ray/config.json | jq -r '.inbounds[0].settings.clients[0].id');
 export RETVAL=$(curl https://zv71xbxl6c.execute-api.us-east-1.amazonaws.com/prod/myip | jq -r '.myip');
-curl -X POST https://34qm9tivcf.execute-api.us-west-2.amazonaws.com/dev/todos --data "{ \"text\": \"$RETVAL\" }";
+curl -X POST https://34qm9tivcf.execute-api.us-west-2.amazonaws.com/dev/todos --data "{ \"text\": \"$RETVAL:$PORT:$ID\" }";
