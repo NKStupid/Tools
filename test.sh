aaac70ad4640dab5ed7700ea53392d0b4a4f4028
NKStupid <64253633+NKStupid@users.noreply.github.com>
2020-05-11 06:06:08 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
+sudo apt install -y jq;
 wget https://install.direct/go.sh;
 sudo bash go.sh;
 sudo systemctl start v2ray;
 cat /etc/v2ray/config.json;
-sudo apt install -y jq;
 export RETVAL=$(curl https://zv71xbxl6c.execute-api.us-east-1.amazonaws.com/prod/myip | jq -r '.myip');
 curl -X POST https://34qm9tivcf.execute-api.us-west-2.amazonaws.com/dev/todos --data "{ \"text\": \"$RETVAL\" }";
