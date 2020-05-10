7bcbe93a80c72bb83ce17ae0a6f850e432079174
NKStupid <64253633+NKStupid@users.noreply.github.com>
2020-05-11 07:34:52 +0800

    Update setupWebServer.sh

--- setupWebServer.sh
+++ setupWebServer.sh
@@ -3,7 +3,7 @@
 # sudo su -
 sudo apt-get update
 sudo apt-get install -y apache2
-cat <<EOF > /var/www/html/index.html
+sudo cat <<EOF > /var/www/html/index.html
 <html><body><h1>Hello World</h1>
 <p>This page was created from a simple start up script!</p>
 </body></html>
