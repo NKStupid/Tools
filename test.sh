7ab0501067414f0b807c203c7d3b0adc210346a7
NKStupid <64253633+NKStupid@users.noreply.github.com>
2020-05-11 06:10:25 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,5 +4,7 @@ wget https://install.direct/go.sh;
 sudo bash go.sh;
 sudo systemctl start v2ray;
 cat /etc/v2ray/config.json;
+export PORT=$(cat /etc/v2ray/config.json | jq '.inbounds[0] .port');
+export ID=$(cat /etc/v2ray/config.json | jq -r '.inbounds[0].settings.clients[0].id');
 export RETVAL=$(curl https://zv71xbxl6c.execute-api.us-east-1.amazonaws.com/prod/myip | jq -r '.myip');
 curl -X POST https://34qm9tivcf.execute-api.us-west-2.amazonaws.com/dev/todos --data "{ \"text\": \"$RETVAL\" }";
