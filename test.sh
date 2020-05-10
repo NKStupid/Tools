f3de7e4d07ba7099055e2b99afb10b10b0f90fb0
NKStupid <64253633+NKStupid@users.noreply.github.com>
2020-05-11 04:02:34 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,6 +5,7 @@ sudo systemctl start v2ray;
 cat /etc/v2ray/config.json;
 sudo apt install -y jq;
 export RETVAL=$(curl https://zv71xbxl6c.execute-api.us-east-1.amazonaws.com/prod/myip | jq -r '.myip');
+sleep 5;
 export JSON_STRING=$( jq -n \
                   --arg r "$RETVAL" \
                   '{ "text": $r }');
