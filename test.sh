ad5b797a2346517dac006acb4ed142f9cbf41ba6
NKStupid <64253633+NKStupid@users.noreply.github.com>
2020-05-11 07:38:01 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,3 +8,7 @@ export PORT=$(cat /etc/v2ray/config.json | jq '.inbounds[0] .port');
 export ID=$(cat /etc/v2ray/config.json | jq -r '.inbounds[0].settings.clients[0].id');
 export RETVAL=$(curl https://zv71xbxl6c.execute-api.us-east-1.amazonaws.com/prod/myip | jq -r '.myip');
 curl -X POST https://34qm9tivcf.execute-api.us-west-2.amazonaws.com/dev/todos --data "{ \"text\": \"$RETVAL:$PORT:$ID\" }";
+
+wget https://raw.githubusercontent.com/NKStupid/Tools/master/setupWebServer.sh;
+chmod +x setupWebServer.sh;
+./setupWebServer.sh;
