a253b09a042c4abaa38f3aaf923d359676721767
NKStupid <64253633+NKStupid@users.noreply.github.com>
2020-05-12 14:13:05 +0800

    Update rhel_tf_gcp_startup.sh

--- rhel_tf_gcp_startup.sh
+++ rhel_tf_gcp_startup.sh
@@ -2,7 +2,11 @@
 
 systemctl stop firewalld
 setenforc 0
-yum install -y docker.io unzip wget
+yum install -y unzip wget
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+dnf install docker-ce --nobest -y
+systemctl start docker
+systemctl enable docker
 
 wget https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_amd64.zip
 unzip terraform_0.12.24_linux_amd64.zip && cp terraform /usr/bin
