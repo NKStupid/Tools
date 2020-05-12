fb3da9fcbd4726b58b6f8c910b70ac5c6366cc01
NKStupid <64253633+NKStupid@users.noreply.github.com>
2020-05-12 13:55:07 +0800

    Update rhel_tf_gcp_startup.sh

--- rhel_tf_gcp_startup.sh
+++ rhel_tf_gcp_startup.sh
@@ -2,12 +2,7 @@
 
 systemctl stop firewalld
 setenforc 0
-yum install -y docker unzip wget
-
-wget https://raw.githubusercontent.com/NKStupid/Tools/master/setupWebServer.sh;
-chmod +x setupWebServer.sh;
-./setupWebServer.sh;
-
+yum install -y docker.io unzip wget
 
 wget https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_amd64.zip
 unzip terraform_0.12.24_linux_amd64.zip && cp terraform /usr/bin
